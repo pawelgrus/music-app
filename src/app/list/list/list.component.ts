843ded4e4c4aa4dee6c53e7467a73ecc5e9df4c8
pagrus <pawel.grus@assecobs.pl>
2018-12-04 19:51:53 +0100

    subskrybcje

--- src/app/list/list/list.component.ts
+++ src/app/list/list/list.component.ts
@@ -1,32 +1,37 @@
 import { IAlbum } from '../model/iAlbum';
 import { IAlbumFilter } from '../model/iAlbumFilter';
 import { ListService } from '../service/list.service';
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
+  searchSubscription: Subscription;
   searchInited = false;
   albumList: IAlbum[] = [];
   private orginalAlbumList: IAlbum[] = [];
 
   constructor(private listService: ListService, private zone: NgZone) {
-    this.listService.$search.subscribe(event => {
-      this.albumList = this.orginalAlbumList.filter(al => {
-        return event.title !== null && al.title.startsWith(event.title);
-      });
-      console.log(this.albumList);
-      
-    });
   }
 
   ngOnInit() {
     this.orginalAlbumList.push({title: 'custom title', artist: 'artysta', length: 40});
     this.orginalAlbumList.push({title: 'slams atack', artist: 'Peja', length: 200, placeOfRecord: 'Warsaw'});
     this.albumList = this.orginalAlbumList;
+    this.searchSubscription = this.listService.$search.subscribe(event => {
+      this.albumList = this.orginalAlbumList.filter(al => {
+        return event.title !== null && al.title.startsWith(event.title);
+      });
+      console.log('subscribe:' + this.albumList);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubscription.unsubscribe();
   }
 
   onClickSearch() {
